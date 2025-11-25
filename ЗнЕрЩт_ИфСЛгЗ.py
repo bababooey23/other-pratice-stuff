91b100759e420651e79909786f4b137c20cfec29
bababooey23 <80262157+bababooey23@users.noreply.github.com>
2025-11-25 11:27:46 +0300

    Add files via upload

---
+++ ЗнЕрЩт_ИфСЛгЗ.py
@@ -0,0 +1,10 @@
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.create_sheet("Vtcctlp")
+if 'Sheet' in wb.sheetnames:
+    wb.remove(wb['Sheet'])
+ws['A1'] = "Hello"
+ws['B2'] = "everyone"
+ws['C3'] = "in the"
+ws['D4'] = "world"
+wb.save('rfrfirbyc.xlsx')
